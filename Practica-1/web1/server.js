91a1a594336a4cdebb5d4d7da854a6fa1a564618
Ana Cuevas Bravo <acuevas@beta03.aulas.gsyc.urjc.es>
2019-01-29 12:40:46 +0100

    Inicio de tienda

---
+++ Practica-1/web1/server.js
@@ -0,0 +1,12 @@
+var http = require('http');
+var fs = require('fs');
+
+console.log("Arrancando servidor...")
+
+http.createServer(function (req, res) {
+  fs.readFile('index.html', function(err, data){
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end(data);
+  console.log("Peticion atendida")
+});
+}).listen(8080);

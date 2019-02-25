9de481382cceff5ac824b5689249db6a2001a3bb
Ana Cuevas Bravo <acuevas@beta03.aulas.gsyc.urjc.es>
2019-02-25 12:55:56 +0100

    CSS

--- Practica-2/mi_tienda/mi_tienda/settings.py
+++ Practica-2/mi_tienda/mi_tienda/settings.py
@@ -99,7 +99,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_DIR = (os.path.join(BASE_DIR, 'mi_tienda/static'),
+STATIC_URL = "/static/"
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'mi_tienda/static'),
 )

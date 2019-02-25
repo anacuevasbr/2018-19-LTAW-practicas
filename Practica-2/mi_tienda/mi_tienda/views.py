d130b1722ce4786d698523a0b277a9c9282be094
Ana Cuevas Bravo <acuevas@beta03.aulas.gsyc.urjc.es>
2019-02-25 11:40:38 +0100

    Usando templates desde settings

--- Practica-2/mi_tienda/mi_tienda/views.py
+++ Practica-2/mi_tienda/mi_tienda/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.template import Template, Context
+from django.template.loader import get_template
+
 
 def mi_funcion(request):
     html = "Rafa es tonto"
@@ -9,11 +11,11 @@ def mi_funcion(request):
 
 
 def mi_producto(request):
-    fp = open('/home/alumnos/acuevas/2018-19-LTAW-practicas/Practica-2/mi_tienda/mi_tienda/product.html')
-    t = Template(fp.read())
-    fp.close
-    c = Context({'name': 'Lisas boots'}, {'imagen':"https://pm1.narvii.com/6506/1ef0fee244e70761691360e40e20db53077d9a51_hq.jpg"},
-                {'descrip':'Lisas ugly boots in as if its your last'}, {'price': 'more than you can afford'})
+    #fp = open('/home/alumnos/acuevas/2018-19-LTAW-practicas/Practica-2/mi_tienda/mi_tienda/product.html')
+    t = get_template('product.html')
+
+    c = {'name': 'Lisas boots', 'imagen':"https://pm1.narvii.com/6506/1ef0fee244e70761691360e40e20db53077d9a51_hq.jpg",
+                'descrip':'Lisas ugly boots in as if its your last', 'price': 'more than you can afford'}
 
     html = t.render(c)
 

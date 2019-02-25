a2308208bc948c7e770a099623eea9ed1101c550
Ana Cuevas Bravo <acuevas@beta03.aulas.gsyc.urjc.es>
2019-02-25 12:42:02 +0100

    Intento css

--- Practica-2/mi_tienda/mi_tienda/settings.py
+++ Practica-2/mi_tienda/mi_tienda/settings.py
@@ -100,3 +100,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIR = (os.path.join(BASE_DIR, 'mi_tienda/static'),
+)

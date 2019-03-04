463b6790c84987f0709960ae2bd91ae6ba5cff70
Ana Cuevas Bravo <acuevas@beta03.aulas.gsyc.urjc.es>
2019-03-04 12:57:24 +0100

    Error cargando el contenido de la basa de datos

--- Practica-2/mi_proyectoweb/mi_tienda/models.py
+++ Practica-2/mi_proyectoweb/mi_tienda/models.py
@@ -1,6 +1,8 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+class Product (models.Model):
+    name = models.CharField(max_length=200)
+    image = models.CharField(max_length=1000)
+    description = models.CharField(max_length=1000)
+    price = models.CharField(max_length=400)
--- Practica-2/mi_proyectoweb/mi_tienda/views.py
+++ Practica-2/mi_proyectoweb/mi_tienda/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 from django.template import Template, Context
 from django.template.loader import get_template
-import json
+from django.shortcuts import render
+from mi_tienda.models import Product
 
 
 def mi_funcion(request):
@@ -14,22 +15,18 @@ def mi_funcion(request):
 def mi_producto(request, param):
 
     num = int(param)
-    json_data = open('/home/alumnos/acuevas/2018-19-LTAW-practicas/Practica-2/mi_tienda/mi_tienda/inventory.json')
-    data = json.load(json_data)
+    products = Product.objects.all()
+    print(products[0])
 
     t = get_template('product.html')
-    c = data[num-1]
+    #c = products[num-1]
+    c= {}
     html = t.render(c)
 
     return HttpResponse(html)
 
 def index_func(request):
     #fp = open('/home/alumnos/acuevas/2018-19-LTAW-practicas/Practica-2/mi_tienda/mi_tienda/product.html')
-    t = get_template('index.html')
 
-    c = {'name': 'Lisas boots', 'imagen':"https://pm1.narvii.com/6506/1ef0fee244e70761691360e40e20db53077d9a51_hq.jpg",
-                'descrip':'Lisas ugly boots in as if its your last', 'price': 'more than you can afford'}
 
-    html = t.render(c)
-
-    return HttpResponse(html)
+    return render(request, "index.html", {})
